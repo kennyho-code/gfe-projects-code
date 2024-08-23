153d5432c453d3d317a7bcc8bbb5731a81769460
Kenny Ho <kennyho.code@gmail.com>
2024-08-23 10:55:13 -0400

    Finished u newsletter section

--- newsletter-section/src/app/page.tsx
+++ newsletter-section/src/app/page.tsx
@@ -3,11 +3,11 @@ import { ReactNode } from "react";
 
 export default function Home() {
   return (
-    <main className="flex xl:flex-row  flex-col p-4 xl:h-screen">
-      <div className="xl:p-4  flex flex-col justify-center gap-8 ">
-        <h1 className="font-semibold text-3xl">
+    <main className="flex xl:flex-row flex-col p-4 min-h-screen max-w-[1440px]">
+      <div className=" flex xl:mr-4 flex-col justify-center gap-8 xl:w-[592px]">
+        <h2 className="font-semibold text-3xl">
           Get the finest curated abstracts delivery weekly to your inbox
-        </h1>
+        </h2>
 
         <ul className="flex flex-col gap-6">
           <ListItem>
@@ -19,19 +19,24 @@ export default function Home() {
 
         <form className="flex flex-col xl:flex-row gap-2">
           <div className="flex flex-col">
-            <input
-              aria-label="email"
-              type="text"
-              className="border-2 h-[40px] rounded-lg p-6"
-              placeholder="Enter your email"
-            />
+            <div className="flex flex-col gap-6 w-full">
+              <input
+                aria-label="email"
+                type="email"
+                required
+                aria-describedby="email-error"
+                className="border-2 h-[40px] rounded-lg  p-4 leading-5 outline-none"
+                placeholder="Enter your email"
+              />
+              <p id="email-error"></p>
+            </div>
             <p className="font-light text-base text-gray-600">
               We only send you the best!. No spam.
             </p>
           </div>
           <button
             type="submit"
-            className="border-2 rounded-lg px-4 py-2 h-[40px]"
+            className="border-none rounded-lg px-4 py-2 h-[40px] inline-flex justify-center padding-0 items-center leading-5 bg-blue-600 text-white"
           >
             Subscribe
           </button>
@@ -39,14 +44,10 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col justify-center">
-        <Image
+        <img
           alt="Picture of abstract graphics"
           src={"/abstract.jpg"}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          className="rounded-lg xl:mt-0 mt-4"
+          className="rounded-lg xl:mt-0 mt-4   xl:h-[608px] h-[288px] object-cover"
         />
       </div>
     </main>
